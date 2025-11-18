f6d618d7c4ea3f63484cee7a0ba2e0064ab6dbcb
progmamun <mamun333089@gmail.com>
2025-11-18 10:42:12 +0600

    solve-2

--- src/solution.ts
+++ src/solution.ts
@@ -1,15 +1,28 @@
-function formatValue(T) {
-  if (typeof T === 'string') {
-    return T.toUpperCase();
-  }
-  if (typeof T === 'number') {
-    return T * 10;
-  }
-  if (typeof T === 'boolean') {
-    return !T;
-  }
-}
+//TODO Problem 1
+// function formatValue(value: string | number | boolean) {
+//   if (typeof value === 'string') {
+//     return value.toUpperCase();
+//   }
+//   if (typeof value === 'number') {
+//     return value * 10;
+//   }
+//   if (typeof value === 'boolean') {
+//     return !value;
+//   }
+// }
 
-console.log(formatValue('hello'));
-console.log(formatValue(5));
-console.log(formatValue(true));
+// console.log(formatValue('hello'));
+// console.log(formatValue(5));
+// console.log(formatValue(true));
+
+//TODO Problem 2
+// function getLength(value: string | any[]) {
+//   if (typeof value === 'string') {
+//     return value.length;
+//   }
+//   if (Array.isArray(value)) {
+//     return value.length;
+//   }
+// }
+// console.log(getLength('typescript'));
+// console.log(getLength([10, 20, 30, 40]));
